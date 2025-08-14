71a9aa4176feee7512b20d72491858327bc8c670
Yan Xiangtong <xiangtongcz@126.com>
2025-08-14 23:17:49 +0800

    ch8

---
+++ ch8/clock2/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func handleConn(conn net.Conn) {
+	defer conn.Close()
+	for {
+		_, err := io.WriteString(conn, time.Now().Format("2006-01-02 15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn)
+	}
+}
